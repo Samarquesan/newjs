2e5ec001fe76e3c44d67028bff0ad583c236ae48
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2023-12-12 00:21:12 -0300

    novos exercícios

---
+++ CursoJS01/aula0008.js
@@ -0,0 +1,7 @@
+let n1, n2, res
+
+n1 = 20
+n2 = 11
+res = n1 >> 2
+
+console.log(res)
---
+++ aula00010.js
@@ -0,0 +1,8 @@
+let status = "A"
+ res = (status == "A" ? "Ativo" : "Inativo")
+
+ console.log(res)
+
+ //0 = False
+ //1 = True
+ // Teste Lógico ? se verdadeiro : se falso
