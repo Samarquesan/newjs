2bcc1a9428b3c853133f9069a766a6937124aaba
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-01-15 16:42:00 -0300

    template strings

---
+++ aula00056.js
@@ -0,0 +1,7 @@
+const caixa = document.querySelector("#caixa");
+
+const curso = "JavaScript";
+const canal = "CFB Cursos";
+const frase = "Este é o curso de "+ curso + " do canal "+ canal
+
+caixa.innerHTML = frase
