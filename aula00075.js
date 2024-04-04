9e68f843229e039556acd04c8aad91480d7967fa
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-04-04 00:04:15 -0300

    promises 02

--- aula00075.js
+++ aula00075.js
@@ -1,27 +1,35 @@
 const numero = document.getElementById("numero");
+const btn_promessa = document.getElementById("btn_promessa");
 
-let promise = new Promise((accepted, rejected)=>{
-    let resultado = true;
-    let tempo = 3000;
-    setTimeout(()=>{
-        if(resultado){
-            accepted("Deu tudo certo!");
-        }else{
-            rejected("Deu tudo errado!");
-        };
-    }, tempo);
+btn_promessa.addEventListener("click", (evt)=>{
+    numero.innerHTML = "Processando...";
+    promessa()
+        .then((retorno)=>{
+            numero.innerHTML = retorno;
+            numero.classList.remove("erro");
+            numero.classList.add("ok");
+        })
+        
+        .catch((retorno)=>{
+            numero.innerHTML = retorno;
+            numero.classList.add("erro");
+            numero.classList.remove("ok");
+        })
 });
 
-promise.then((retorno)=>{
-    numero.innerHTML = retorno;
-    numero.classList.remove("erro");
-    numero.classList.add("ok");
-});
+const promessa = ()=>{
+    let p = new Promise((accepted, rejected)=>{
+        let resultado = true;
+        let tempo = 3000;
+        setTimeout(()=>{
+            if(resultado){
+                accepted("Deu tudo certo!");
+            }else{
+                rejected("Deu tudo errado!");
+            };
+        }, tempo);
+    });
+    return p
+}
 
-promise.catch((retorno)=>{
-    numero.innerHTML = retorno;
-    numero.classList.add("erro");
-    numero.classList.remove("ok");
-});
-
-numero.innerHTML = "Processando...";
+numero.innerHTML = "Esperando...";
