4eabbcf0162f611c6a7b2942eadbd18780ac8701
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-04-09 17:05:19 -0300

    alarme pronto

--- aula00079.js
+++ aula00079.js
@@ -6,6 +6,32 @@ const tmp_alarme = document.getElementById("tmp_alarme")
 const hora_alarme = document.getElementById("hora_alarme")
 const timer = document.getElementById("timer")
 
+const som_alarme = new Audio("efeitos_sonoros/Digital Watch Alarm Long.mp3")
+som_alarme.loop =-1
+
+let ts_atual =null
+let ts_alarme = null
+let alarme_ativado = false
+let alarme_tocando = false
+
+btn_ativar.addEventListener("click", (evt)=>{
+    ts_atual = Date.now()
+    ts_alarme = ts_atual + (tmp_alarme.value * 1000)
+    alarme_ativado = true
+    const dt_alarme = new Date(ts_alarme)
+    hora_alarme.innerHTML = "Hora do alarme: "+dt_alarme.getHours()+ ":" + dt_alarme.getMinutes()+":" +dt_alarme.getSeconds()
+})
+
+btn_parar.addEventListener("click", (evt)=>{
+    alarme_ativado = false
+    alarme_tocando = false
+    hora_alarme.innerHTML = "HOra do alarme:"
+    timer.classList.remove("alarme")
+    som_alarme.pause()
+    som_alarme.currentTime = 0
+})
+
+
 const data = new Date()
 
 let dia = data.getDate()
@@ -26,6 +52,13 @@ const relogio = ()=>{
     const hora_completa = hora + ":" +  minutos + ":" +  segundos
     console.log(hora_completa)
     div_relogio.innerHTML = hora_completa
+    if(alarme_ativado && !alarme_tocando){
+        if(data.getTime() >= ts_alarme){
+            alarme_tocando = true
+            som_alarme.play()
+            timer.classList.add("alarme")
+        }
+    }
 }
 
 const intervalo = setInterval(relogio, 1000)
