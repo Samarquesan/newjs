a72dde105541c81b69633b585639f786851a3c92
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-04-20 01:17:39 -0300

    math lib

---
+++ aula00080.js
@@ -0,0 +1,6 @@
+const mat = document.getElementById("mat");
+const num = Math.floor(Math.random()*10) + 1; // Método da biblioteca math que gera um número aleatório 
+
+//mat.innerHTML = Math.round(num); // Método que aredonda para baixo um número
+//mat.innerHTML = Math.pow(10,3); // para potencias
+mat.innerHTML = num
