ee0ddd715c6608d403309e822cf9e411ffd85404
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-05-14 17:19:43 -0300

    objetos literais

--- aula00090.js
+++ aula00090.js
@@ -1,9 +1,9 @@
 const objetos = document.getElementById("objetos");
 
 const computador = {
-    cpu: "AMD5",
-    ram: "64bg",
-    hd: "2tb",
+    cpu: "",
+    ram: "",
+    hd: "",
      info: function(){
         console.log(`CPU: ${this.cpu}`)
         console.log(`RAM: ${this.ram}`)
@@ -13,8 +13,23 @@ const computador = {
 
 computador["monitor"]= "22pol";
 computador.placaVideo = "rtx";
+delete(computador.hd);
 
-console.log(computador["monitor"]);
+const c1 = Object.assign({}, computador);
+console.log(computador);
+c1.info();
+
+const c2 = Object.create(computador);
+c2.cpu= "i9";
+c2.ram = "32gb";
+c2.hd = "2tb";
+c2.info();
+
+const o1 = {obj1:"1"};
+const o2 = {obj2:"1"};
+const o3 = {obj3:"1"};
+const o4 = Object.assign(o1,o2,o3);
+console.log(o4);
 
 const computadores = [
     {
