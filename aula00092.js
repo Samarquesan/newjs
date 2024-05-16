8cbfb20ec1187c794ebefb1e91e213c76dd6d67d
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-05-15 22:51:35 -0300

    módulos

---
+++ aula00092.js
@@ -0,0 +1,7 @@
+import { cursos } from "./cursos.js";
+import { carros } from "./cursos.js";
+
+console.log("Servidor rodando!");
+console.log(cursos);
+console.log(carros);
+
---
+++ cursos.js
@@ -0,0 +1,5 @@
+const cursos = ["JavaScript", "HTML", "CSS", "Arduino", "Rasberry", "C++", "Python", "Java", "C#"];
+const carros = ["Polo", "T-Cross", "Golf", "Onix", "Cruze", "Argo", "Cronnos"];
+
+export {cursos};
+export{carros};
