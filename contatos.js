a394c9b0d4cd62ec3a65e2c43675b16890f6afc8
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-05-18 20:13:38 -0300

    modulos 7

--- contatos.js
+++ contatos.js
@@ -12,21 +12,27 @@ let contato = {
             nome: novoContato.nome,
             telefone: novoContato.telefone,
             email: novoContato.email
-        }
-        contatos.push(cont)
-        const div = document.createElement("div");
-        div.setAttribute("class", "contato");
-        const p_nome = document.createElement("p");
-        p_nome.innerHTML = novoContato.nome;
-        const p_telefone = document.createElement("p");
-        p_telefone.innerHTML = novoContato.telefone;
-        const p_email = document.createElement("p");
-        p_email.innerHTML = novoContato.email;
-        div.appendChild(p_nome);
-        div.appendChild(p_telefone);
-        div.appendChild(p_email);
-        destinoDOM.appendChild(div);
+        };
+        contatos.push(cont);
+
+        destinoDOM.innerHTML = "";
+
+        contatos.forEach((c)=>{
+            const div = document.createElement("div");
+            div.setAttribute("class", "contato");
+            const p_nome = document.createElement("p");
+            p_nome.innerHTML = c.nome;
+            const p_telefone = document.createElement("p");
+            p_telefone.innerHTML = c.telefone;
+            const p_email = document.createElement("p");
+            p_email.innerHTML = c.email;
+            div.appendChild(p_nome);
+            div.appendChild(p_telefone);
+            div.appendChild(p_email);
+            destinoDOM.appendChild(div);
+        });
+        
     }
-}
+};
 
 export default contato;
