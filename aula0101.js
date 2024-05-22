293803a1618281a092b2aa032ceb6bd644f4a2aa
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-05-21 23:50:01 -0300

    manipulação de strings 02

--- aula0101.js
+++ aula0101.js
@@ -1,4 +1,5 @@
-const nome = new String("Bruno Marques");
+const nome = new String("Samuel Marques dos Santos");
+let nome2 = new String("Bruno Marques")
 let canal = new String("CFBCursos");
 
 console.log(nome);
@@ -11,3 +12,12 @@ console.log(nome.concat(canal))
 console.log(nome)
 console.log(nome.indexOf("u"))
 console.log(nome.lastIndexOf("u"))
+console.log(nome.localeCompare(nome2))
+console.log(nome.replace("Santos", "Silva"))
+console.log(nome.search("dos"))
+
+let lastName = nome.slice(19,25)
+console.log(lastName)
+
+let arrNome = nome.split(" ")
+console.log(arrNome)
