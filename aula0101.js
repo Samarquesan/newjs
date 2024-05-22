31a15d12e33f1122b0403dc6f0b2642403660384
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-05-21 22:56:20 -0300

    maniplução de strings 01

---
+++ aula0101.js
@@ -0,0 +1,13 @@
+const nome = new String("Bruno Marques");
+let canal = new String("CFBCursos");
+
+console.log(nome);
+console.log(typeof(nome));
+
+console.log(nome.charAt(4))
+console.log(nome.charCodeAt(1))
+
+console.log(nome.concat(canal))
+console.log(nome)
+console.log(nome.indexOf("u"))
+console.log(nome.lastIndexOf("u"))
