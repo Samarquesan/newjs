60c77663ab07399b69ee5c577ea0d8f40bc30514
Samarquesan <sam.samarques28@gmail.com>
2025-06-30 23:09:13 -0300

    Funções de Manipulação de strings

---
+++ aula104.js
@@ -0,0 +1,21 @@
+let nome=new String("Samuel Marques dos Santos")
+
+console.log(nome.startsWith("Sa"))
+
+console.log(nome.endsWith("os"))
+
+console.log(nome.includes("Cassia"))
+
+console.log(nome.repeat(4))
+console.log(nome.valueOf())
+
+console.log(nome.charCodeAt(0))
+console.log(nome.charCodeAt(1))
+console.log(nome.charCodeAt(2))
+console.log(nome.charCodeAt(3))
+console.log(nome.charCodeAt(4))
+console.log(nome.charCodeAt(5))
+console.log(String.fromCodePoint(83, 97, 109, 117, 101, 108))
+
+let nome_ts="Samuel"
+console.log(`Nome: ${nome_ts}`)
