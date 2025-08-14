b41162449cb300b2c2f2e2be9c5c698e87072853
Samarquesan <sam.samarques28@gmail.com>
2025-08-13 23:53:57 -0300

    RegEX

--- aula105.js
+++ aula105.js
@@ -1,4 +1,7 @@
-let nome=new String("Bruno Pinho Campos")
+let nome=new String("Bruno Pinho Campos 1978")
+let email= "bruno@bruno.com.br"
+let numeros= "1, 10, 100, 1000"
+
 console.log(nome)
 
 console.log(nome.search(/pinho/i))//RegEx
@@ -6,3 +9,18 @@ console.log(nome.search(/pinho/i))//RegEx
 console.log(nome.match(/O/ig))
 
 console.log(nome.replace(/o/ig, "a"))
+
+console.log(nome.match(/[a-g|h-z|0-9]/ig))
+
+//Metacaracteres
+console.log(nome.match(/\d/ig))//Números 
+console.log(nome.match(/\s/ig))//Espaços
+console.log(nome.match(/\bP/ig))//DWORD
+
+//Quantificadores
+console.log(numeros)
+console.log(nome.match(/o+|s+/ig))
+console.log(numeros.match(/10/ig))
+console.log(numeros.match(/10+/ig))
+console.log(numeros.match(/10*/ig))
+console.log(numeros.match(/10?/ig))
