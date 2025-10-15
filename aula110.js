e56c2f393be0ec1d5c012322892ca1ede63fcd13
Samarquesan <sam.samarques28@gmail.com>
2025-10-15 19:45:31 -0300

    Aplicando condicionais de limite de tela

--- aula110.js
+++ aula110.js
@@ -4,16 +4,37 @@ const btn_esquerda=document.getElementById("btn_esquerda")
 const btn_direita=document.getElementById("btn_direita")
 
 const init=() =>{
-    carro.style="position:relative;left:0px"
+    carro.style="position:relative;left:0px;width:100px"
+    tamMax= window.innerWidth - parseInt(carro.style.width)
     
 }
 
 let anima= null
+let tamMax= null
 
 const move=(dir)=>{
-    carro.style.left=parseInt(carro.style.left) + (10*dir) + "px"
-    anima = setTimeout(move, 20, dir)
+    if(dir > 0){
+        if(parseInt(carro.style.left) <= tamMax){
+            carro.style.left=parseInt(carro.style.left) + (10*dir) + "px"
+            anima = setTimeout(move, 20, dir)
+        }else{
+            clearTimeout(anima)
+        }
+    }else if(dir < 0){
+        if(parseInt(carro.style.left) >= 0){
+            carro.style.left=parseInt(carro.style.left) + (10*dir) + "px"
+            anima = setTimeout(move, 20, dir)
+        }else{
+            clearTimeout(anima)
+        }
+    }
+
+    console.log(tamMax)
+    
 }
+    //carro.style.left=parseInt(carro.style.left) + (10*dir) + "px"
+    
+
 
 btn_parar.addEventListener("click", ()=>{
     clearTimeout(anima)
@@ -26,9 +47,12 @@ btn_esquerda.addEventListener("click", () =>{
 
 btn_direita.addEventListener("click", () =>{
     clearTimeout(anima)
-    move(1)
+    move(1 )
     
 })
 
-window.onload=init
-//window.addEventListener("load", init())
+//window.onload=init
+window.addEventListener("load", init())
+window.addEventListener("resize", ()=>{
+    tamMax= window.innerWidth - parseInt(carro.style.width)
+})
