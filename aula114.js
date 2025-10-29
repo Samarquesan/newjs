c8cb45589d12eea8a51d7a44f91f066ab8e5bad9
Samarquesan <sam.samarques28@gmail.com>
2025-10-29 18:56:07 -0300

    desestruturação

---
+++ aula114.js
@@ -0,0 +1,22 @@
+let numeros =()=>{
+    return [10, 20, 30, 40]
+}
+
+// let a = 10;
+// let b = 20;
+
+// [a, b]=[b, a]
+
+//let numeros = [10, 20, 30, 40]
+// let [a, b, c, d]= numeros
+//({a, b, c, d} = {a:"Verde", b:"Amarelo", c:"Azul", d:"Branco"})
+
+// console.log(a)
+// console.log(b)
+
+let [a, b, c, d]=numeros()
+
+ console.log(a)
+ console.log(b)
+ console.log(c)
+ console.log(d)
