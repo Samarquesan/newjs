39f701f7283e7360fff9751c504bc46c9bc049a0
Samarquesan <sam.samarques28@gmail.com>
2025-11-11 16:43:09 -0300

    casos do uso obrigatório do ponto e virgula

---
+++ aula118.js
@@ -0,0 +1,26 @@
+//[, (
+// let n1=[1, 2]
+// let n2=[3, 4];
+// [n1, n2].map(v=>console.log(v))
+
+// let n= null
+// (n=10)
+// console.log(n)
+
+let numeros = [10, 20, 30];
+[n1, n2,n2, n3]=numeros
+console.log(n1)
+console.log(n2)
+console.log(n3)
+
+// let canal="CFB Cursos";
+
+// ['J', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p', 't'].forEach(l=>console.log(i));
+
+// let i = null; i=0
+// console.log(i)
+
+// let i = null
+// for(i = 0; i < 10; i++);
+
+// console.log(i)
