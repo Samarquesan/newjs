752eb175c6e8b08ec241bc73507b04b536bfc280
Samarquesan <sam.samarques28@gmail.com>
2025-11-18 17:30:21 -0300

    Aula de validação parte 2

--- aula119.js
+++ aula119.js
@@ -3,12 +3,16 @@ const f_nota=document.querySelector("#f_nota");
 const f_msg=document.querySelector("#f_msg");
 
 document.querySelector("#btn_validar").addEventListener("click", (evt)=>{
-    let msg = null
-
-    if(!f_nota.checkValidity()){
-        msg=f_nota.validationMessage
+    let msg= null
+    if(f_nota.validity.valueMissing){
+        msg= "Este campo precisa ser preenchido!"
+        
+    }else if(f_nota.validity.rangeOverflow){
+        msg="A Nota registrada é muito alta!"
+    }else if(f_nota.validity.rangeUnderflow){
+        msg="A Nota registrada é muito baixa!"
     }
-
-    f_msg.innerHTML=msg
+    f_nota.reportValidity()
+    f_msg.innerHTML= msg
     evt.preventDefault()
 })
