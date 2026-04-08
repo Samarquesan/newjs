6e1a9b62662ee232203d63bfbf8f6f68de399585
Samarquesan <sam.samarques28@gmail.com>
2026-04-07 21:00:40 -0300

    Timestamp
    criando um cronometro

---
+++ aula129.js
@@ -0,0 +1,14 @@
+const timer=document.querySelector("#timer");
+
+const tmpIni= Date.now();
+
+const contador=()=>{
+    const tmpAtual= Date.now()
+    let cont = tmpAtual-tmpIni
+    let seg= Math.floor((tmpAtual-tmpIni)/1000)
+    console.log(seg)
+}
+
+setInterval(contador,1000)
+
+contador()
